40b78d557d83c06a5ff22272cbc43400f977f265
3salaz <3salaz.dev@gmail.com>
2021-06-02 14:28:51 -0700

    added materialize

---
+++ public/assets/scripts/firebaseLoad.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const loadEl = document.querySelector('#load');
+    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+    
+    try {
+      let app = firebase.app();
+      let features = [
+        'auth', 
+        'database', 
+        'firestore',
+        'functions',
+        'messaging', 
+        'storage', 
+        'analytics', 
+        'remoteConfig',
+        'performance',
+      ].filter(feature => typeof app[feature] === 'function');
+      console.log(`Firebase SDK loaded with ${features.join(', ')}`);
+    } catch (e) {
+      console.error(e);
+      loadEl.textContent = 'Error loading the Firebase SDK, check the console.';
+    }
+  });
